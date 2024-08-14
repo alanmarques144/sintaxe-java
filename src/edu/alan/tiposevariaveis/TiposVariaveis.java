dafb84d05f6bdd5ec9bfd9049eba6176f1686101
alanmarques144 <alanshalom12@gmail.com>
2024-08-14 11:12:38 -0300

    Tipos de dados e valores no java

--- src/edu/alan/tiposevariaveis/TiposVariaveis.java
+++ src/edu/alan/tiposevariaveis/TiposVariaveis.java
@@ -3,7 +3,24 @@ package edu.alan.tiposevariaveis;
 public class TiposVariaveis {
 
     public static void main(String[] args) {
+
+        String nome = "DONALD MCDONALD";
+        double valor = 3123.14;
+        byte idade = 22;
+        short ano = 2021;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        int numero = 5;
+        final double VALOR_CONSTANTE = 144; // NÃO É POSSIVEL MODIFICAR O VALOR DA VARIAVÉL
+
+        //observando o fato do java ser fortemente tipado
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
         
+        
+
     }
     
 }
