2c57288022edce12431d587f909c7f297010d99b
alanmarques144 <alanshalom12@gmail.com>
2024-08-16 09:39:37 -0300

    fazendo tratamento de exceções

---
+++ src/edu/alan/terminalexcecoes/CepInvalidoException.java
@@ -0,0 +1,5 @@
+package edu.alan.terminalexcecoes;
+
+public class CepInvalidoException extends Exception {
+    
+}
--- src/edu/alan/terminalexcecoes/MinhaClasse.java
+++ src/edu/alan/terminalexcecoes/MinhaClasse.java
@@ -1,4 +1,4 @@
-package edu.alan.terminaleargumentos;
+package edu.alan.terminalexcecoes;
 
 public class MinhaClasse {
     public static void main(String[] args) {
